fbe418d7ccd5241e97640c23ad22978db28c254b
Vitaly Volkov <v.volkov@takeprofit.com>
2025-06-12 13:00:24 +0000

    reversed import statement

--- backend/tsapp/src/index.ts
+++ backend/tsapp/src/index.ts
@@ -1,6 +1,4 @@
-// import { generate_uuid as gu } from '@backend/mylib';
-import MyLib from '@backend/mylib';
-const { generate_uuid: gu } = MyLib;
+import { generate_uuid as gu } from '@backend/mylib';
 
 let id: string = gu();
 console.log("Hello, from TypeScript app! Generated id=" + id);
