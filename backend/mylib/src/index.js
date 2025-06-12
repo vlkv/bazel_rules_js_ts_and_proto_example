af2ae5d5055cbb90bcf45b3efba6f17359f5507d
Vitaly Volkov <v.volkov@takeprofit.com>
2025-06-12 11:44:03 +0000

    tsapp works

--- backend/mylib/src/index.js
+++ backend/mylib/src/index.js
@@ -1,4 +1,8 @@
 
 const MyLib = require('./mylib');
 
+// CommonJS export (for backward compatibility)
 module.exports.generate_uuid = MyLib.generate_uuid;
+
+// ES6 export (for TypeScript)
+// export const generate_uuid = MyLib.generate_uuid;
--- backend/tsapp/src/index.ts
+++ backend/tsapp/src/index.ts
@@ -1,4 +1,6 @@
-import { generate_uuid as gu } from '@backend/mylib';
+// import { generate_uuid as gu } from '@backend/mylib';
+import MyLib from '@backend/mylib';
+const { generate_uuid: gu } = MyLib;
 
 let id: string = gu();
 console.log("Hello, from TypeScript app! Generated id=" + id);
