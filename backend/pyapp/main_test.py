f628ac20fa8caac99bf49cafb032c20cb762e3e2
Vitaly Volkov <v.volkov@takeprofit.com>
2025-07-23 13:31:01 +0000

    fix

--- backend/pyapp/main_test.py
+++ backend/pyapp/main_test.py
@@ -1,7 +1,7 @@
 import sys
 print(f'PYTHONPATH={sys.path}')
 
-from .main import say_hello_to
+from main import say_hello_to
 from example.thing.thing_pb2 import Thing
 from example.other.other_api_pb2 import OtherRequest, OtherResponse
 from example.other.other_api_pb2_grpc import OtherApiStub
@@ -9,6 +9,6 @@ from example.other.other_api_pb2_grpc import OtherApiStub
 def test_say_hello_to():
     assert say_hello_to('Foo') == 'Hello, Foo!'
     req = OtherRequest(name='foo')
-    assert req.name == 'foo123'
+    assert req.name == 'foo'
     th = Thing()
     assert th
